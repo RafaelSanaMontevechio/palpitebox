3553706d6cc564ddcdd79e25a782164bcf150c5a
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2020-09-25 15:05:17 -0300

    fix loading

--- components/Loading/index.js
+++ components/Loading/index.js
@@ -5,7 +5,8 @@ import loading from './loading.module.css';
 const Loading = () => {
   return (
     <div className={loading.container}>
-      <div className={loading.loading}></div>
+      <div className={loading.loading}></div>&nbsp;
+      <span className={loading.message}> Aguarde, carregando promoção...</span>
     </div>
   );
 };
